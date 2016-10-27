71a7bf54012f2b995a76ecf34a2ca66bf89e6ff0
mlarson <larson.mathew@gmail.com>
2016-10-26 18:23:14 -0700

    timezone issue

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
 	blank=True, null=False)
 
 	def publish(self):
-		self.published_date = timezone.new()
+		self.published_date = timezone.now()
 		self.save()
 
 	def __str__(self):
